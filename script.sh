4de378d721a99ef4ae011f3753d67c226b364c8a
Tanveer J <77818526+mjan2021@users.noreply.github.com>
2022-11-03 18:49:33 -0400

    bash Script added

--- script.sh
+++ script.sh
@@ -1,1 +1,14 @@
-python .\LogExtraction.py -id 2047 -f data_storage.json --vfolder Z:/VIDEOS/2047/Video/ --lfolder Z:/VIDEOS/2047/Disk_files/debug/
+#!/bin/bash
+
+storage='data_storage.json'
+root='Z:/VIDEOS/'
+
+for id in 1001 1002 1005 1006 1009 1010
+do
+#  python .\LogExtraction.py -id $id -f $storage --vfolder $root$id/Video --lfolder $root$id/Disk_files/debug/
+ echo "--vfolder $root$id/Video"
+ echo "--lfolder $root$id/Disk_files/debug/"
+# sleep 3
+ wait
+done
+#python .\LogExtraction.py -id 2047 -f data_storage.json --vfolder Z:/VIDEOS/2047/Video/ --lfolder Z:/VIDEOS/2047/Disk_files/debug/
