f180a4421355bbda0b1b40131b6440173f341876
Tanveer J <mjan2021@fau.edu>
2023-09-27 11:04:27 -0400

    changes

--- access_server.sh
+++ access_server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Accessing Web-Server on HPC
-ssh mjan2021@boc22-h-ivsdccoa.hpc.fau.edu
+#ssh mjan2021@boc22-h-ivsdccoa.hpc.fau.edu
+Ssh mjan2021@131.91.174.21
 
 # Repo Directory
 # cd /opt/Driver_indices/
