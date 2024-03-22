b4f00305d7b3bddb6ba1ae4ccce6789284e5710b
Tanveer J <mjan2021@fau.edu>
2024-03-22 15:25:34 -0400

    optimizers changed

--- CustomGenderDetection.py
+++ CustomGenderDetection.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import EfficientNetB7
+from tensorflow.keras.applications.efficientnet_v2 import EfficientNetV2M
 from tensorflow.keras.applications.convnext import ConvNeXtBase
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers.experimental import Adamax, Adadelta, Adagrad, Ftrl, Nadam, RMSprop, SGD
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -44,7 +46,8 @@ validation_generator = train_datagen.flow_from_directory(
 
 # Load EfficientNetB0 base model
 # base_model = EfficientNetB7(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-base_model = ConvNeXtBase(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+# base_model = ConvNeXtBase(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+base_model = EfficientNetV2M(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 base_model.trainable = False
 
 # Add custom classification head
@@ -60,7 +63,7 @@ for layer in base_model.layers:
     layer.trainable = False
 
 # Compile the model
-model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy',])
+model.compile(optimizer=Adadelta(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy',])
 
 # Train the model
 history = model.fit(
@@ -70,4 +73,4 @@ history = model.fit(
 )
 
 # Save the trained model
-model.save('./gender_split/convnext-base.h5')
+model.save('./gender_split/efficientnet_v2m.h5')
